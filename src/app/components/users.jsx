b22c507f15127b42398aaf7d802fb566a13afb08
HerrKise <oskarwilde777@gmail.com>
2021-09-22 20:57:50 +0300

    Initial commit

---
+++ src/app/components/users.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
+import User from "./user";
+import api from "../api";
+import GroupList from "./groupList";
+const Users = ({ users: allUsers, ...rest }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfession] = useState();
+    const [selectedProf, setSelectedProf] = useState();
+    const count = allUsers.length;
+    const pageSize = 4;
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfession(data));
+    }, []);
+
+    const handleProfessionSelect = (item) => {
+        setSelectedProf(item);
+    };
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const filtredUsers = selectedProf
+        ? allUsers.filter((user) => user.profession === selectedProf)
+        : allUsers;
+    const usersCrop = paginate(filtredUsers, currentPage, pageSize);
+    return (
+        <>
+            {professions && (
+                <GroupList
+                    selectedItem={selectedProf}
+                    items={professions}
+                    onItemSelect={handleProfessionSelect}
+                />
+            )}
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usersCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+Users.propTypes = {
+    users: PropTypes.array
+};
+
+export default Users;
